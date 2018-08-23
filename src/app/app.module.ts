1777ed3a463c009eaef52df6b7da25c03483195b
poorna14 <poorna.14@cse.mrt.ac.lk>
2018-08-23 09:47:48 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,10 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-
+import {SettingsPage} from '../pages/settings/settings';
+import{Geolocation} from '@ionic-native/geolocation';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -15,9 +15,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   declarations: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SettingsPage
+  
   ],
   imports: [
     BrowserModule,
@@ -27,14 +28,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    SettingsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    Geolocation,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component,ViewChild,ElementRef} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Geolocation} from '@ionic-native/geolocation';
+
+declare let google:any;
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage {
+  lat:any;
+  lng:any;
+  map:any;
+@ViewChild('map') mapRef:ElementRef;
+
+  constructor(public navCtrl: NavController,public geo: Geolocation) {
+  }
+
+  ionViewDidLoad(){
+  	this.showMap();
+  	this.geo.getCurrentPosition().then( pos =>{
+  		this.lat=pos.coords.latitude;
+  		this.lng=pos.coords.longitude;
+  	}).catch(err => console.log(err))
 
   }
+
+  showMap(){
+  	const location = new google.maps.LatLng(this.lat,this.lng);
+
+  	const options = {
+  		timeout: 20000,
+		enableHighAccuracy: true,
+  		center:location,
+  		zoom :20,
+  		mapTypeId: google.maps.MapTypeId.ROADMAP
+  	}
+
+  	this.map = new google.maps.Map(this.mapRef.nativeElement,options);
+
+  }
+
 
 }
